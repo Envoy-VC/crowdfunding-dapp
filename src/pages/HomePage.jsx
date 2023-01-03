7893b80eed9e0e8a287e502ad7aa3f71454d17c6
Vedant Chainani <vedantchainani1084@gmail.com>
2023-01-04 00:23:44 +0530

    Add Home Page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const HomePage = () => {
-  return <div>Page</div>;
+import { DisplayCampaigns } from "../components";
+import { useStateContext } from "../context";
+
+const Home = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([]);
+
+  const { address, contract, getCampaigns } = useStateContext();
+
+  const fetchCampaigns = async () => {
+    setIsLoading(true);
+    const data = await getCampaigns();
+    setCampaigns(data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (contract) fetchCampaigns();
+  }, [address, contract]);
+
+  return (
+    <DisplayCampaigns
+      title="All Campaigns"
+      isLoading={isLoading}
+      campaigns={campaigns}
+    />
+  );
 };
 
-export default HomePage;
+export default Home;
