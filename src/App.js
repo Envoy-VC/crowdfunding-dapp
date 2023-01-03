4f8164a3506e6dfe0b03faf1d9a70bf029bee5bc
Vedant Chainani <vedantchainani1084@gmail.com>
2023-01-03 18:56:36 +0530

    Create Template Pages

--- src/App.js
+++
@@ -1,6 +0,0 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
-import "./styles/Home.css";
-
-export default function Home() {
-  return <div className="container">App</div>;
-}
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as HomePage } from "./HomePage";
+export { default as Profile } from "./Profile";
+export { default as CreateCampaign } from "./CreateCampaign";
+export { default as CampaignDetails } from "./CampaignDetails";
