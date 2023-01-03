8fb270686448b3d850bb89108a51ea89a5a4b5c1
Vedant Chainani <vedantchainani1084@gmail.com>
2023-01-03 18:52:27 +0530

    Add Create Campaign UI Components

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Sidebar } from "./Sidebar";
+export { default as Navbar } from "./Navbar";
+export { default as CustomButton } from "./CustomButton";
+export { default as FormField } from "./FormField";
+export { default as Loader } from "./Loader";
--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { StateContextProvider } from "./context";
+
+import App from "./App";
 import "./styles/globals.css";
 
-// This is the chainId your dApp will work on.
-const activeChainId = ChainId.Mumbai;
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const container = document.getElementById("root");
-const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-    <ThirdwebProvider desiredChainId={activeChainId}>
-      <App />
-    </ThirdwebProvider>
-  </React.StrictMode>
+  <ThirdwebProvider desiredChainId={ChainId.Mumbai}>
+    <Router>
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
+    </Router>
+  </ThirdwebProvider>
 );
